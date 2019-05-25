13f612354f657df897afabbdb367f49423d73936
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2019-05-25 18:33:05 +0200

    Fix package.json after installing storybook

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 
-import styled from 'styled-components';
-
 import GlobalStyle from '../styles/global';
-
-import Button from './molecules/atoms/Button';
-
-const TestSpace = styled.div`
-  background: black;
-  padding: 4em;
-`;
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <TestSpace>
-      <Button.Raised>Sign up</Button.Raised>
-      <Button.Raised invertedBg>Sign up</Button.Raised>
-    </TestSpace>
-    <h1>Welcome to Liquidator.</h1>
-    <Button.Raised inverted>Sign up</Button.Raised>
+    <h1>Bienvenue, добро пожаловать, velkommen, willkommen, welcome to Liquidator.</h1>
   </>
 );
 
